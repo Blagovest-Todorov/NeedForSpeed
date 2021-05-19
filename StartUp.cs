3da0b5cdd2f351ee9bb7acaab95f9c15a70feb0f
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-19 08:48:21 +0300

    add projerct files

---
+++ StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SportCar sportCar = new SportCar(100, 50);
+            sportCar.Drive(200);
+            Console.WriteLine($"{sportCar.GetType().Name} {sportCar.Fuel}");
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelCosumption = 1.25;
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.Fuel = fuel;
+            this.HorsePower = horsePower;
+        }
+
+        public int HorsePower { get; set; }
+        public double Fuel { get; set; }        
+
+        public virtual double FuelConsumption => DefaultFuelCosumption;  // per kms 
+
+        public virtual void Drive(double kilometers) 
+        {
+            this.Fuel = this.Fuel - kilometers * this.FuelConsumption;
+        }
+    }
+}
